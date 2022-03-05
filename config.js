69fc7459055f5c342940f82098afca87b34e62c8
Jhony Dev <iamjareco@gmail.com>
2022-03-05 10:36:04 +0100

    Added eslint configuration and fix all

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    env: process.env,
-    endpoint: {
-        auth: '/api/auth',
-        user: '/api/user'
-    }
+  env: process.env,
+  endpoint: {
+    auth: '/api/auth',
+    user: '/api/user'
+  }
 }
