69ec76c8b573d57d58c16fb4cd48f7f66aa22d6a
Jhony Dev <iamjareco@gmail.com>
2022-03-05 12:34:06 +0100

    Added user schema and controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,21 @@
 const { request, response } = require('express');
+const User = require('../models/user');
+
+// const checkUser = async (req = request, res = response) => {
+//   const { email } = req.body;
+//   const user = User.findOne({ $eq: email });
+//   console.log(user);
+// };
 
 const createUser = async (req = request, res = response) => {
-  return res.status(201).send({ user: req.body });
+  const user = new User(req.body);
+
+  try {
+    const userSaved = await user.save();
+    res.status(201).send({ user: userSaved });
+  } catch (error) {
+    res.status(500).send({ error: 'Error to save user' });
+  }
 };
 
 module.exports = {
---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  lastname: {
+    type: String,
+    required: [true, `The lastname is required`],
+  },
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  registerDate: {
+    type: Date,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    default: 'INACTIVE',
+    enum: ['ACTIVE', 'INACTIVE'],
+  },
+
+  role: {
+    type: String,
+    default: 'USER_ROLE',
+    enum: ['ADMIN_ROLE', 'USER_ROLE'],
+  },
+
+  lastSession: { type: Date },
+  birthday: { type: Date },
+  locale: { type: String },
+});
+
+module.exports = model('User', UserSchema);
