ff754e83f2077c8088d2dc7e7961eeb61f7438cc
Jhony Dev <iamjareco@gmail.com>
2022-04-05 21:55:38 +0200

    Routes has been refactored

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
-const headersSchema = require('../schemas/headers.validator');
-const validator = require('../middlewares/validator');
+const { customLogger } = require('../middlewares/logger');
+const { checkToken } = require('../middlewares/securityToken');
 const { endpoint } = require('../config');
 
 const router = express.Router();
 
 // Main
-router.use(endpoint.base, validator('headers', headersSchema.secure));
+router.use(endpoint.base, customLogger);
 
 // Routes
 router.use(endpoint.auth, require('../routes/auth.routes'));
-router.use(endpoint.user, require('../routes/user.routes'));
+router.use(endpoint.user, checkToken, require('../routes/user.routes'));
 
 module.exports = router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const { createUser } = require('../controllers/user.controller');
 const validator = require('../middlewares/validator');
 const userValidator = require('../schemas/user.validator');
+const {
+  findUserById,
+  getUsers,
+  updateUser,
+  removeUser,
+} = require('../controllers/user.controller');
 
 const router = express.Router();
 
-// router.get('/:id', validator('params', userValidator.get), findUser);
-router.post('/register', validator('body', userValidator.post), createUser);
-// router.put('/update', validator('body', userValidator.put), updateUser);
-// router.delete('/:id', validator('params', userValidator.delete), removeUser);
-
-// WITH MIDDLEWARE
-// router.put("/account/update", securityMiddleware.checkToken, userController.updateUser);
-// router.get("/account/logout", userController.logOutUser);
-// router.delete("/remove/account", userController.deleteUser);
+router.get('/list', getUsers);
+router.get('/:id', validator('params', userValidator.get), findUserById);
+router.put('/account', validator('body', userValidator.put), updateUser);
+router.delete('/:id', validator('params', userValidator.delete), removeUser);
 
 module.exports = router;
